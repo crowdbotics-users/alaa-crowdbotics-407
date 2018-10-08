957516a4b1e104624c35672649ad8b065226f045
André Machado <csantos.machado@gmail.com>
2018-10-08 17:30:37 -0300

    Added additional settings to heroku.

--- foodtalk/settings/staging.py
+++ foodtalk/settings/staging.py
@@ -2,4 +2,4 @@ from .base import *
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
