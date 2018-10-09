962e88e9b00a2844723c4aea77677a8610234423
André Machado <csantos.machado@gmail.com>
2018-10-09 19:35:37 -0300

    set gdal/geos lib env again.

--- foodtalk/settings/staging.py
+++ foodtalk/settings/staging.py
@@ -3,5 +3,3 @@ from .base import *
 MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware"]
 ALLOWED_HOSTS = ["*"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-GDAL_LIBRARY_PATH = os.environ.get("GDAL_LIBRARY_PATH")
-GEOS_LIBRARY_PATH = os.environ.get("GEOS_LIBRARY_PATH")
