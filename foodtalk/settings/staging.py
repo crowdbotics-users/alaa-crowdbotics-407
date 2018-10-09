8450cd7621f32ee8502054cbf196531556bb4b32
André Machado <csantos.machado@gmail.com>
2018-10-09 18:18:10 -0300

    Added gdal_library_path var.

--- foodtalk/settings/staging.py
+++ foodtalk/settings/staging.py
@@ -4,3 +4,4 @@ MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 ALLOWED_HOSTS = ['*']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
