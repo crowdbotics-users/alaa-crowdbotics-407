005105a552f0df1afb4841876252eaba5325b12c
André Machado <csantos.machado@gmail.com>
2018-10-15 18:16:34 -0300

    Fixed settings for s3 on staging.

--- foodtalk/settings/staging.py
+++ foodtalk/settings/staging.py
@@ -4,5 +4,5 @@ MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware"]
 ALLOWED_HOSTS = ["*"]
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'alaa-crowdbotics-407.core.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStorage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
